f2d32b0275c9a33c4a62c57dff6bb3c37795dcfd
stnqls <stnqls3938@gmail.com>
2021-06-01 21:59:21 +0900

    전개연산자

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-//구조 분해 할당 == 비구조화 할당
+//전개 연산자
 
-const user = {
-  name : 'Heropy',
-  age : 85,
-  email : 'thesecon@gmail.com',
-  address : 'USA'
+const fruits = ['Apple', 'Banana', 'Cherry','Orange']
+console.log(fruits)
+console.log(...fruits) //String 형태로 출력된다. = console.log('Apple','Banana','Cherry')
+
+function toObject(a,b, ...c) {
+  return {
+    a: a, 
+    b: b, 
+    c: c
+  }
 }
-const {name:heropy, age, email, address = 'Korea'} = user
+console.log(toObject(...fruits)) //= console.log(toObject(fruits[0], fruits[1], fruits[2]))
 
-console.log(`사용자의 이름은 ${heropy} 입니다.`)
-console.log(`${heropy}의 나이은 ${age} 입니다.`)
-console.log(`${heropy}의 이메일 주소는 ${email} 입니다.`)
-console.log(address)
-
-const fruits = ['Apple', 'Banana', 'Cherry']
-const [a,b,c,d] = fruits
-console.log(a,b,c,d)
+// = const toObject= (a,b, ...c)=> {a,b,c}
