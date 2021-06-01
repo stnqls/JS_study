3603b3fbc8ca0b9e7a5b8246296d48e4b130f26a
stnqls <stnqls3938@gmail.com>
2021-06-01 22:22:10 +0900

    깊은복사1

--- main.js
+++ main.js
@@ -13,3 +13,9 @@
 
  console.log('------')
  console.log('------')
+
+ //갚은 복사
+ user.emails.push('neo@zillinks.com')
+ console.log(user.emails === copyUser.emails)
+ console.log('user', user)
+ console.log('copyUser', copyUser)
