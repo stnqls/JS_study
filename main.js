e7945dbe47edce3d9e8e48c3f87e486419094f4e
stnqls <stnqls3938@gmail.com>
2021-06-01 18:20:57 +0900

    this

--- main.js
+++ main.js
@@ -1,45 +1,55 @@
+//this
+//일반 함수는 호출 위치에 따라 this정의
+//화살표 함수는 자신이 선언된 함수 범위에서 this정의
+
 const heropy = {
-  firstName : 'Heropy',
-  lastName:'Park',
-  getFullName: function (){
-    return `${this.firstName} ${this.lastName}`
+  name: 'Heropy',
+  normal: function(){
+    console.log(this.name)
+  },
+  arrow: () => {
+    console.log(this.name)
   }
 }
-console.log(heropy.getFullName())
 
-const amy ={
-  firstName: 'Amy',
-  lastName : 'Clarke',
-  getFullName: function (){
-    return `${this.firstName} ${this.lastName}`
+heropy.normal()
+heropy.arrow()
+
+const amy = {
+  name : 'Amy',
+  normal : heropy.normal,
+  arrow : heropy.arrow
+}
+amy.normal()
+amy.arrow()
+
+
+
+
+function User(name) {
+  this.name = name
+}
+User.prototype.normal = function () {
+  console.log(this.name)
+}
+User.prototype.arrow = () => {
+  console.log(this.name)
+}
+
+const Heropy = new User('Heropy')
+
+Heropy.normal()
+Heropy.arrow()
+
+
+
+const timer = {
+  name : 'Heropy!!',
+  timeout : function(){
+    //setTimeout(함수, 시간)
+    setTimeout( ()=>{
+      console.log(this.name)
+    },2000)
   }
 }
-console.log(amy.getFullName())
-
-const neo ={
-  firstName: 'Neo',
-  lastName : 'Smith',
-  getFullName: function (){
-    return `${this.firstName} ${this.lastName}`
-  }
-}
-console.log(neo.getFullName())
-
-
-
-// 클래스
-
-function User(first, last) {
-  this.firstName = first
-  this.lastName = last
-}
-
-User.prototype.getFullName = function (){
-  return `${this.firstName} ${this.lastName}`
-}
-
-const subin = new User('subin','seon') //생성자 함수
-const youngbin = new User('youngbin','seon')
-
-console.log(subin.getFullName())
-console.log(youngbin)
+timer.timeout()
