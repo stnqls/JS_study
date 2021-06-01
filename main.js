245f926082620f79f045dc171e6830e6f6e7b4e5
stnqls <stnqls3938@gmail.com>
2021-06-01 15:41:28 +0900

    data type

---
+++ main.js
@@ -0,0 +1,16 @@
+import getType from './getType'
+
+console.log('hello world!');
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+
+
+console.log(getType(123));
+console.log(getType(false));
+console.log(getType(null));
+console.log(getType({}));
+console.log(getType([]));
