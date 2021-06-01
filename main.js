c725f4f5eca68259851d2a6400fbc1faf5abb4d4
stnqls <stnqls3938@gmail.com>
2021-06-01 16:21:09 +0900

    조건문

--- main.js
+++ main.js
@@ -1,58 +1,29 @@
-// 산술연산자
+import random from './getRandom'
 
-console.log(1 + 2)
-console.log(5 - 7)
-console.log(3 * 4)
-console.log(10 / 2)
-console.log(7 % 5)
+//조건문 (for)
 
-//할당 형산자
+const a = random()
 
-let num = 2
-//a= a + 1
-num += 1
-// += -= *= /= %=
-console.log(num)
-
-//비교 연산자
-
-const a = 1
-const b = 1
-const c = 2
-console.log("a==b는 ", a == b)
-console.log("a!==b는 ", a !== b)
-console.log("a==c는 ",a == c)
-// <, >, >=, <=, ==, !==, 
-
-function isEqual(x,y){
-  return x == y
+if(a == 0){
+  console.log('a is 0')
+}else if (a == 2){
+  console.log('a is 2')
+}else{
+  console.log('rest...')
 }
-console.log(isEqual(1,1))
-console.log(isEqual(2,'2'))
 
-//논리 연산자
+//조건문 (swirch)
 
-const x = 1==1
-const y = 'ab' == 'ab'
-let z = true
-
-console.log(x)
-console.log(y)
-console.log(z)
-
-console.log('&&: ',x && y && z) //and연산자
- z = false
- console.log('&&: ',x && y && z)
- console.log('||: ',x || y || z) //or연산자
- console.log('!: ',!x) //not연산자
-
- //삼항 연산자
- const num1 = 1<2
-
- if(num1) {
-   console.log('참')
- }else {
-   console.log('거짓')
- }
-
- console.log(a ? '참' : '거짓')
+switch (a){
+  case 0:
+    console.log('a is 0')
+    break
+  case 2 :
+    console.log('a is 2')
+    break
+  case 4:
+    console.log('a is 4')
+    break
+  default :
+  console.log('rest...')
+}
