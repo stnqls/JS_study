cf951610c31e99a11254d6ac12ebd2dd381a9ff6
stnqls <stnqls3938@gmail.com>
2021-06-01 16:47:04 +0900

    형변환

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-// 변수 유효범위
-// var let const
+//형 변환
 
-function scope(){
-  if(true){
-    const a = 123
-    console.log(a)
-  }
+const a= 1
+const b ='1'
+
+console.log(a === b) // ===일치 연산자 ==동등 연산자
+console.log(a==b)
+
+//Truthy (참 같은 값)
+// true , {}, [], 1,2,'false', -12, '3.14'
+
+//Falsy(거짓 같은 값)
+// false, '', null, undefined, 0, -0, NaN
+
+if(true){
+  console.log(123)
 }
-scope()
-// let, const는 블록 레벨을 갖는다.
-// var는 함수범위의 유효범위를 갖는다.
+if(''){
+  console.log(123)
+}
