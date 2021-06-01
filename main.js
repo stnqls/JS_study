c83c08027fe0318f8043098ebd7a6b973b65d93b
stnqls <stnqls3938@gmail.com>
2021-06-01 21:42:22 +0900

    객체

--- main.js
+++ main.js
@@ -1,107 +1,32 @@
-const numbers = [1,2,3,4]
-const fruits =['Apple', 'Banana', 'Cherry']
+const userAge ={
+  name : 'Heropy',
+  age: 85
+}
 
-console.log(numbers[1]) //2
-console.log(fruits[2]) //Cherry
+const userEmail = {
+  name :'Heropy',
+  email : 'thesecon@gmail.com'
+}
 
-//length
+const target = Object.assign({},userAge, userEmail) // 맨 앞에 빈 객체 데이터 {}를 입력할 경우 합쳐진 데이터가 새로운객체데이터를 만든다.
+console.log(target)
+console.log(userAge)
+console.log(target === userAge) //참조형 : 객체, 배열, 함수
 
-console.log(numbers.length)
-console.log(fruits.length)
-console.log([1,2].length)
-console.log([].length)
-
-//concat
-
-console.log(numbers.concat(fruits)) //원본에는 영향 x
-
-//forEach
-
-fruits.forEach(function (element, index, array) {
-  console.log(element,index,array)
-})
-
-//map
-
-const a = fruits.forEach((fruit,index) => {
-  console.log(`${fruit}-${index}`)
-})
-console.log(a)
-
-const b = fruits.map(function (fruit, i){ //반환된 값을 모아서 새로운 배열로 만들어 준다.
-  return {
-    id: i,
-    name: fruit
-  }
-})
-console.log(b)
-
-//화살표 함수로 표현
-// const b = fruits.map((fruit, i)=>({
-//   id: i,
-//   name: fruit
-// }))
-// console.log(b)
+const a = {k : 123}
+const b = {k : 123}
+console.log(a === b) //다른 메모리를 참조 하고 있다.
 
 
-//filter
+const user = {
+  name : 'SUBIN',
+  age: 80,
+  email : 'tn9612@naver.com'
+}
+const keys = Object.keys(user)
+console.log(keys)
 
-const a2 = numbers.map(number => {
-  return number < 3
-})
-console.log(a2)
+console.log(user['email'])
 
-const b2 = numbers.filter(number => {
-  return number < 3
-})
-console.log(b2)
-console.log(numbers)
-
-
-//find(), findIndex
-
-const a3 = fruits.find(fruit => {
-  return /^B/.test(fruit)
-})
-console.log(a3)
-
-const a4 = fruits.findIndex(fruit => {
-  return /^C/.test(fruit)
-})
-console.log(a4)
-
-
-//includes
-
-const a5 = numbers.includes(3)
-console.log(a5)
-
-const a6 = fruits.includes('HEROPY')
-console.log(a6);
-
-//push , unshift 원본 수정됨 주의
-
-numbers.push(5)//뒤에 데이터 추가
-console.log(numbers)
-
-numbers.unshift(0) //앞에 데이터 추가
-console.log(numbers)
-
-//reverse 원본 수정됨 주의
-
-numbers.reverse()
-fruits.reverse()
-
-console.log(numbers)
-console.log(fruits)
-
-
-//splice() 원본 수정됨 주의
-
-numbers.reverse();fruits.reverse();
-
-numbers.splice(2,1, 999)
-console.log(numbers)
-
-fruits.splice(2,0,'Orange')
-console.log(fruits)
+const values = keys.map(key => user[key])
+console.log(values)
