9120fd1f4ddfd4a2489d3da656dfcbada0e24036
stnqls <stnqls3938@gmail.com>
2021-06-01 22:16:51 +0900

    얕은 복사1

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-//데이터 불변성
-// 원시데이터 : String, Number, Boolean, undefined, null
-let a = 1
-let b = 4
-console.log(a, b, a === b)
-b = a
-console.log(a, b, a === b)
-a = 7
-console.log(a, b, a === b)
-let c = 1
-console.log(b, c, b === c)
+// 얕은 복사
+ const user = {
+   name : 'Heropy',
+   age : 85,
+   emails : ['thesecon@gmail.com']
+ }
+ const copyUser = user
+ console.log(copyUser === user)
 
-//참조형 데이터 : Object, Array ,Function
-let x = {k : 1}
-let y = {k : 1}
-console.log(x, y, x === y)
-x.k = 7
-y = x
-console.log(x, y, x === y)
-let z = y
-console.log(x, y, z, x === z)
-x.k = 9
-console.log(x, y, z, x === z)
+ user.age = 22
+ console.log('user',user)
+ console.log('copyUser', copyUser)
+
+ console.log('------')
+ console.log('------')
