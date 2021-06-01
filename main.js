f007c46df257e1f8a008cb7c559ae0befff4aea1
stnqls <stnqls3938@gmail.com>
2021-06-01 16:31:16 +0900

    반복문

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-import random from './getRandom'
+//반복문
+// for(시작조건; 종료조건; 변화조건)
 
-//조건문 (for)
+const ulEl = document.querySelector('ul')
 
-const a = random()
-
-if(a == 0){
-  console.log('a is 0')
-}else if (a == 2){
-  console.log('a is 2')
-}else{
-  console.log('rest...')
+for(let i = 0; i < 10; i += 1){
+  const li = document.createElement('li')
+  li.textContent=`list - ${i + 1} `
+  if((i + 1) % 2 == 0){
+    li.addEventListener('click', function(){
+      console.log(li.textContent)
+    })
+  }
+  ulEl.appendChild(li)
 }
 
-//조건문 (swirch)
-
-switch (a){
-  case 0:
-    console.log('a is 0')
-    break
-  case 2 :
-    console.log('a is 2')
-    break
-  case 4:
-    console.log('a is 4')
-    break
-  default :
-  console.log('rest...')
-}
