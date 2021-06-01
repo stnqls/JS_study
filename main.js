fcb8e560fb1e47afc9c6e2de67c2cc5a3a7124c6
stnqls <stnqls3938@gmail.com>
2021-06-01 17:33:55 +0900

    Timer 함수

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-//호이스팅 Hoisting
-//함수 선언부가 유효범위 최상단으로 끌어 올려지는 현상
+//타이머 함수
+// setTimeout(함수, 시간) : 일정시간 후 함수 실행
+// setInterval(함수, 시간) : 시간 간격마다 함수 실행
+//clearTimeout(): 설정된 Timeout 함수를 종료
+//clearInterval(): 설정된 Interval 함수를 종료
 
-const a =7
+setTimeout(function(){
+  console.log('Heropy!')
+}, 3000)
+
+const timer = setTimeout(()=>{
+  console.log('heropy!')
+},3000);
+
+const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  clearTimeout(timer)
+});
 
 
-double()
+const timerInterval = setInterval(()=>{
+  console.log('heropy!')
+},3000);
 
-function double (){
-  console.log(a * 2)
-}
+const h1ElInterval = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  clearInterval(timerInterval)
+});
