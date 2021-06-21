6752c55b842d7ac203dfc02598196a576034e32d
stnqls <stnqls3938@gmail.com>
2021-06-21 18:15:58 +0900

    배열 내장함수-shift,pop,unshift,push

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 //배열 내장함수
 
 const numbers = [10,20,30,40];
+ 
+//shift : 원본을 변화시키며, 첫번째 원소를 추출한다.
+const value = numbers.shift();
+console.log(value); //10 
 
-//slice 원본 배열을 바꾸지 않는다.
-const sliced = numbers.slice(0,2);
-console.log(sliced); // 10,20
-console.log(numbers); // 10,20,30,40
+//pop : 원본을 변화시키며, 마지막 원소를 추출한다.
+const value2 = numbers.pop();
+console.log(value2);
 
-// splice: 특정항목을 제거할때 사용한다. 원본이 변한다.
-const index = numbers.indexOf(30);
-numbers.splice(index,1);
-console.log(numbers); // 10,20,40
-numbers.splice(index,2);
-console.log(numbers); // 10,20
+//unshift : 제일 첫번째 자리에 값을 추가한다.
+numbers.unshift(5);
+console.log(numbers); // 5,10,20,30,40
+
+//push : 제일 마지막 자리에 값을 추가한다.
+numbers.push(50);
+console.log(numbers) // 10,20,30,40,50
