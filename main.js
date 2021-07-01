4a2ecccf39fd41dc2f246143b19f01dc31ca3cd5
stnqls <stnqls3938@gmail.com>
2021-07-01 22:17:51 +0900

    Truthy & Falsy

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-// 삼항연산자 condition ? true : false
+// Truthy & Falsy
 
-const array =[];
-/*
-let text = '';
-if (array.length ===0) {
-  text = '배열이 비어있습니다.';
-}else {
-  text = '배열이 비어있지 않습니다.';
-}
-*/
-let text = array.length === 0 ? '배열이 비어있습니다.' : '배열이 비어있지 않습니다.';
-console.log(text);
+//Falsy
+console.log(!undefined);
+console.log(!null);
+console.log(!0);
+console.log(!'');
+console.log(!NaN); // Not a Number
+console.log(!false);
 
-const condition1 = false;
-const condition2 = false;
-const value = condition1 ? '와우!' : condition2 ? 'blabla' : 'foo'; //중첩해서 사용할 경우,if문이 더 좋다.
-console.log(value);
+//Truthy
+console.log(!3);
+console.log(!'hello');
+console.log(!['array']);
+console.log(![]);
+console.log(!{});
+console.log(!true);
+
+const value = {};
+const truthy = !!value;
+console.log(truthy);
