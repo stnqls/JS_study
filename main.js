0eddb9d88da78c2e422329ecfedfdb666436122c
stnqls <stnqls3938@gmail.com>
2021-07-01 21:55:56 +0900

    Class

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 // 프로토타입과 클래스
-// 상속
+// ES6 class
 
-function Animal(type, name, sound) {
-  this.type = type;
-  this.name = name;
-  this.sound = sound;
+class Animal {
+  constructor(type, name, sound) { //constructor생성자라는 의미를 가지고 있다.
+    this.type = type;
+    this.name = name;
+    this.sound = sound;
+  }
+  say(){ //자동으로 prototype으로 변환해 준다.
+    console.log(this.sound);
+  }
 }
 
-Animal.prototype.say = function() {
-  console.log(this.sound);
-}
+// const dog = new Animal('개','멍멍이','멍멍');
+// const cat = new Animal('고양이','야옹이','야옹');
 
-function Dog (name, sound) {
-  Animal.call(this, '개', name, sound);
+class Dog extends Animal {
+  constructor(name,sound) {
+    super('개',name,sound);
+  }
 }
-
-function Cat (name, sound) {
-  Animal.call(this, '고양이', name, sound);
+class Cat extends Animal {
+  constructor(name,sound) {
+    super('고양이',name,sound);
+  }
 }
-
-Dog.prototype = Animal.prototype;
-Cat.prototype = Animal.prototype;
 
 const dog = new Dog('멍멍이','멍멍');
 const cat = new Cat('야옹이','야옹');
-
 dog.say();
 cat.say();
