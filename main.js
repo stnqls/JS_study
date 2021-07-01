fc92bb38794510837d9d3b070464c5c3163fc6bf
stnqls <stnqls3938@gmail.com>
2021-07-01 21:48:13 +0900

    객체생성자 상속하기

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 // 프로토타입과 클래스
-// 객체 생성
+// 상속
 
 function Animal(type, name, sound) {
   this.type = type;
   this.name = name;
   this.sound = sound;
-
- /* this.say= function () {
-    console.log(this.sound);
-  }*/
 }
 
-Animal.prototype.say = function() { //공유할수 있는 값이나 함수를 prototype으로 설정한다.
+Animal.prototype.say = function() {
   console.log(this.sound);
 }
 
-const dog = new Animal('개','멍멍이','멍멍');
-const cat = new Animal('고양이','야옹이','야옹');
+function Dog (name, sound) {
+  Animal.call(this, '개', name, sound);
+}
+
+function Cat (name, sound) {
+  Animal.call(this, '고양이', name, sound);
+}
+
+Dog.prototype = Animal.prototype;
+Cat.prototype = Animal.prototype;
+
+const dog = new Dog('멍멍이','멍멍');
+const cat = new Cat('야옹이','야옹');
 
 dog.say();
 cat.say();
