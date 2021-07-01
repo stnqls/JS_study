9451bfc5142a9cf484c7ae8581367b1e5c765b9a
stnqls <stnqls3938@gmail.com>
2021-07-01 22:40:00 +0900

    함수의 파라미터

--- main.js
+++ main.js
@@ -1,57 +1,15 @@
-//단축평가 논리 계산법
+// 함수 파라미터
 
-true && true; //true
-true && false; //false
-true || false; //true
-true || true; // true
-
-const dog = {
-  name: '멍멍이'
-};
-
-function getName(animal) {
- /*
-  if (animal) {
-    return animal.name;
-  } 
-  return undefined;
-  */
- return animal && animal.name;
+function calculateCircleArea(r) {
+  const radius = r || 1; //r이 정해지지 않았을 경우 기본값으로 1을 사용한다.
+  return Math.PI *radius * radius;
 }
-
-const name = getName(dog);
-console.log(name);
-
-console.log(true && 'hello'); //hello가 출력된다.
-console.log(false && 'hello') //flase
-console.log('hello' && 'bye'); // bye
-console.log(null && 'hello'); //null
-console.log(undefined && 'hello')//undefined
-
-
-const namelessDog = {
-  name : ''
-};
-
-function getName2(animal) {
-  const name = animal && animal.name;
-  /*
-  if (!name) {
-    return '이름이 없는 동물입니다.';
-  }
-  return name;
-  */
-  return name || '이름이 없는 동물입니다.'
+// 1. 파라미터 기본값으로 설정하기
+function calculateCircleArea(r = 1) {
+  return Math.PI *r * r;
 }
+// 2. 화살표 함수로 만들기
+const calculateCircleArea = (r = 1) => Math.PI * r * r;
 
-const name2 = getName2(namelessDog);
-console.log(name2);
-
-//값이 존재하지 않을때 대신해서 사용할때 유용하다.
-console.log(false || 'hello'); //hello
-console.log('' || 'hello'); //hello
-console.log(null || 'hello'); //null
-console.log(undefined || 'hello'); //hello
-
-console.log(1 || 'hello'); //1
-console.log(true || 'hello'); //true
+const area = calculateCircleArea();
+console.log(area);
