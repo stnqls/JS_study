4c40e81f6c7d2f5bea8a92311e4cbb54f2a820cd
stnqls <stnqls3938@gmail.com>
2021-07-01 22:34:35 +0900

    단축 평가 논리 계산법

--- main.js
+++ main.js
@@ -1,21 +1,57 @@
-// Truthy & Falsy
+//단축평가 논리 계산법
 
-//Falsy
-console.log(!undefined);
-console.log(!null);
-console.log(!0);
-console.log(!'');
-console.log(!NaN); // Not a Number
-console.log(!false);
+true && true; //true
+true && false; //false
+true || false; //true
+true || true; // true
 
-//Truthy
-console.log(!3);
-console.log(!'hello');
-console.log(!['array']);
-console.log(![]);
-console.log(!{});
-console.log(!true);
+const dog = {
+  name: '멍멍이'
+};
 
-const value = {};
-const truthy = !!value;
-console.log(truthy);
+function getName(animal) {
+ /*
+  if (animal) {
+    return animal.name;
+  } 
+  return undefined;
+  */
+ return animal && animal.name;
+}
+
+const name = getName(dog);
+console.log(name);
+
+console.log(true && 'hello'); //hello가 출력된다.
+console.log(false && 'hello') //flase
+console.log('hello' && 'bye'); // bye
+console.log(null && 'hello'); //null
+console.log(undefined && 'hello')//undefined
+
+
+const namelessDog = {
+  name : ''
+};
+
+function getName2(animal) {
+  const name = animal && animal.name;
+  /*
+  if (!name) {
+    return '이름이 없는 동물입니다.';
+  }
+  return name;
+  */
+  return name || '이름이 없는 동물입니다.'
+}
+
+const name2 = getName2(namelessDog);
+console.log(name2);
+
+//값이 존재하지 않을때 대신해서 사용할때 유용하다.
+console.log(false || 'hello'); //hello
+console.log('' || 'hello'); //hello
+console.log(null || 'hello'); //null
+console.log(undefined || 'hello'); //hello
+
+console.log(1 || 'hello'); //1
+console.log(true || 'hello'); //true
