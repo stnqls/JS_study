e5dc5783c6a8e917d4c2f56b1a6721301c365062
stnqls <stnqls3938@gmail.com>
2021-07-04 00:01:04 +0900

    spread연산자

--- main.js
+++ main.js
@@ -1,62 +1,30 @@
-// 비구조화 할당 (구조분해)
+// spread연산자
+//기존 객체를 복사하고 추가적인 값을 입력할때 사용한다.
 
-//기본값 설정
-const object = {a: 1};
-const {a,b} = object;
-console.log(a); //1
-console.log(b); //undefined
-
-function print({ a, b = 2}) {
-  console.log(a); 
-  console.log(b); 
-}
-print(object); // 1, 2
-
-//이름바꾸기
-const animal = {
-  name :'멍멍이',
-  type :'개'
-};
-const {name: nickname} = animal; // = const nickname = animal.name;
-console.log(nickname);
-
-//배열 비구조화 할당, 기본값 설정
-const array = [1];
-const [one, two = 2] = array; //const one = array[0]; const two = array[1];
-
-console.log(one);
-console.log(two);
-
-//객체의 깊은 값꺼내기
-const deepObject = {
-  state: {
-    information: {
-      name: 'subin',
-      languages: ['korean','english','chinese']
-    }
-  },
-  value: 5
+const slime = {
+  name: '슬라임'
 }
 
-// 1. 비구조화 2번 사용하기
-const {name, languages} = deepObject.state.information;
-const {value} = deepObject;
+const cuteSlime = {
+  name: '슬라임',
+  attribute: 'cute'
+}
 
-//비구조화할당 1번 사용하기
-const {
-  state : {
-    information :{
-      name,
-      languages : [first, second]
-    }
-  },
-  value
-} = deepObject;
+const purpleCuteSlime = {
+  ...cuteSlime,
+  color: 'purple'
+}
 
-const extracted = {
-  name, // name : name, languages : languages 키의 값이 이미 있는경우 값을 생략해서 사용 가능하다.
-  first,
-  second,
-  value
-};
-console.log(extracted);
+console.log(slime);
+console.log(cuteSlime);
+console.log(purpleCuteSlime);
+
+const animals = ['개','고양이','참새'];
+const anotherAnimals = [...animals, '비둘기'] // = animals.concat('비둘기');와 같다.
+
+console.log(animals); //개, 고양이, 참새
+console.log(anotherAnimals); // 개, 고양이, 참새, 비둘기
+
+const numbers = [1,2,3,4,5];
+const spreadNumbers = [...numbers, 1000, ...numbers];
+console.log(spreadNumbers); // 1, 2, 3, 4, 5, 1000, 1, 2, 3, 4, 5
