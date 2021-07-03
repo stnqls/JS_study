b35c54ce6d512b5f29a3bfbb9793508f77af201a
stnqls <stnqls3938@gmail.com>
2021-07-04 00:49:23 +0900

    Hoisting

--- main.js
+++ main.js
@@ -1,40 +1,10 @@
-//scope
-// 변수, 함수를 사용할때 해당하는 범위
-//1. global : 전역범위, 코드의 모든 범위에서 사용가능하다.
-//2. function : 코드의 특정 함수 내부에서 사용가능하다.
-//3. block : if문 for문에 쓰이는 '{}' 안에서 사용 가능하다.
-
-const value = 'hello!'; //global
-
-function myFunction(){ 
-  console.log('myFunction: ');
-  console.log(value);
-}
-
-function otherFunction(){
-  console.log('otherFunction: ');
-  const value = 'bye!'; //function
-  console.log(value);
-}
+//Hoisting
+//함수 또는 변수(var)가 선언시 최 상단으로 끌어 올려지는 현상
+//const나 let은 호이스팅이 발생하지 않는다.
+// 호이스팅은 최대한 피해서 작성해야한다.
 
 myFunction();
-otherFunction();
 
-console.log('global scope: ');
-console.log(value);
-
-function yourFunction(){
-  const value = 'bye!';
-if(true) {
-  const value = 'world';
-  console.log('block scope : ');
-  console.log(value); //block
+function myFunction (){
+  console.log('hello world');
 }
-console.log('function scope: ');
-console.log(value);
-}
-
-yourFunction();
-console.log('global scope: ');
-console.log(value); // hello!
-console.log(anotherValue); // error
